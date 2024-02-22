368165bdb8fc801c6d0f2c44d774d59daae9670f
Leshi Chen <leshi@pdx.edu>
2024-02-22 15:06:58 -0800

    fix small typo in memo

--- final_project/memoization.java
+++ final_project/memoization.java
@@ -9,14 +9,13 @@ public class memoization {
         if (str1.charAt(m - 1) == str2.charAt(n - 1))
             dp[m][n] = dfs(str1, str2, m - 1, n - 1, dp) + str1.charAt(m - 1);
         else {
-            String left = dfs(str1, str2, m, n - 1, dp);
-            String up = dfs(str1, str2, m - 1, n, dp);
+            String up = dfs(str1, str2, m, n - 1, dp);
+            String left = dfs(str1, str2, m - 1, n, dp);
             dp[m][n] = (left.length() > up.length() ? left : up);
         }
         return dp[m][n];
     }
 
-    // Top-down DP
     public static String memoizationSolution(String str1, String str2) {
         int m = str1.length();
         int n = str2.length();
