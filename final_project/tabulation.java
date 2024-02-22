456aa393eeb95e5329793235e99a6b197c27a171
Leshi Chen <leshi@pdx.edu>
2024-02-22 15:07:24 -0800

    changed the tab solution be more consistent as memo

--- final_project/tabulation.java
+++ final_project/tabulation.java
@@ -4,30 +4,20 @@ public class tabulation {
     public static String tabulationSolution(String str1, String str2) {
         int m = str1.length();
         int n = str2.length();
-        int[][] dp = new int[m + 1][n + 1];
+        String[][] dp = new String[m + 1][n + 1];
+
+        // Initialize the dp table
+        for (int i = 0; i <= m; i++)
+            for (int j = 0; j <= n; j++)
+                dp[i][j] = "";
 
         for (int i = 1; i <= m; i++)
             for (int j = 1; j <= n; j++)
                 if (str1.charAt(i - 1) == str2.charAt(j - 1))
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    dp[i][j] = dp[i - 1][j - 1] + str1.charAt(i - 1);
                 else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = (dp[i - 1][j].length() > dp[i][j - 1].length()) ? dp[i - 1][j] : dp[i][j - 1];
 
-        // return dp[m][n];
-        // Build the LCS string from the dp table
-        StringBuilder lcs = new StringBuilder();
-        int i = m, j = n;
-        while (i > 0 && j > 0) {
-            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                lcs.append(str1.charAt(i - 1));
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                i--;
-            } else {
-                j--;
-            }
-        }
-        return lcs.reverse().toString();
+        return dp[m][n];
     }
 }
