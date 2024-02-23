938f1c362962f0832054a35d5a4193d45d3f1544
Leshi Chen <leshi@pdx.edu>
2024-02-22 22:41:42 -0800

    fixed implementation error in tabulation space optimization solution

--- final_project/tabulationSpaceOptimization.java
+++ final_project/tabulationSpaceOptimization.java
@@ -1,31 +1,29 @@
 package final_project;
 
-import java.util.Arrays;
-
 public abstract class tabulationSpaceOptimization {
     public static String tabulationSpaceOptimizationSolution(String str1, String str2) {
         int m = str1.length();
         int n = str2.length();
-        String lcs = "";
 
-        // Initializing 2 arrays of size m
-        int[] prev = new int[m + 1];
-        int[] curr = new int[m + 1];
-
-        for (int i = 1; i < m + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                // If matching
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    curr[j] = 1 + prev[j - 1];
-                    lcs += str1.charAt(i - 1);
-                }
-                // Not matching
-                else
-                    curr[j] = Math.max(curr[j - 1], prev[j]);
-            }
-            prev = Arrays.copyOf(curr, m + 1);
+        // Initializing the dp table
+        String[] prevRow = new String[n + 1];
+        String[] currRow = new String[n + 1];
+        for (int i = 0; i <= n; i++) {
+            prevRow[i] = "";
+            currRow[i] = "";
         }
 
-        return lcs;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++)
+                // If matching
+                if (str1.charAt(i - 1) == str2.charAt(j - 1))
+                    currRow[j] = prevRow[j - 1] + str1.charAt(i - 1);
+                // Not matching
+                else
+                    currRow[j] = (prevRow[j].length() > currRow[j - 1].length()) ? prevRow[j] : currRow[j - 1];
+            prevRow = currRow.clone();
+        }
+
+        return currRow[n];
     }
 }
