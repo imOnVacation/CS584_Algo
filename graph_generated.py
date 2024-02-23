f01bd0b05489a2387f57b01b1e6d0a1a544d513e
Leshi Chen <leshi@pdx.edu>
2024-02-22 22:40:40 -0800

    added graph for tabulation space optimization

--- graph_generated.py
+++ graph_generated.py
@@ -7,15 +7,23 @@ data = pd.read_csv("runtime_memory_data.csv")
 # Separate data for Tabulation and Memoization algorithms
 tabulation_data = data[data['Algorithm'] == 'Tabulation']
 memoization_data = data[data['Algorithm'] == 'Memoization']
+tabulation_space_optimization_data = data[data['Algorithm']
+                                          == 'Tabulation Space Optimization']
+
 
 # Plotting Time Usage
 plt.figure(figsize=(10, 6))
 plt.subplot(1, 2, 1)
 
 plt.plot(tabulation_data['Test Name'],
-         tabulation_data['Time (nanoseconds)'], marker='o', label='Tabulation')
+         tabulation_data['Time (nanoseconds)'],
+         marker='o', label='Tabulation')
 plt.plot(memoization_data['Test Name'],
-         memoization_data['Time (nanoseconds)'], marker='o', label='Memoization')
+         memoization_data['Time (nanoseconds)'],
+         marker='o', label='Memoization')
+plt.plot(tabulation_space_optimization_data['Test Name'],
+         tabulation_space_optimization_data['Time (nanoseconds)'],
+         marker='o', label='Tabulation Space Optimization')
 
 plt.xlabel('Test Name')
 plt.ylabel('Time (nanoseconds)')
@@ -27,9 +35,14 @@ plt.legend()
 plt.subplot(1, 2, 2)
 
 plt.plot(tabulation_data['Test Name'],
-         tabulation_data['Memory Used (bytes)'], marker='o', label='Tabulation')
+         tabulation_data['Memory Used (bytes)'],
+         marker='o', label='Tabulation')
 plt.plot(memoization_data['Test Name'],
-         memoization_data['Memory Used (bytes)'], marker='o', label='Memoization')
+         memoization_data['Memory Used (bytes)'],
+         marker='o', label='Memoization')
+plt.plot(tabulation_space_optimization_data['Test Name'],
+         tabulation_space_optimization_data['Memory Used (bytes)'],
+         marker='o', label='Tabulation Space Optimization')
 
 plt.xlabel('Test Name')
 plt.ylabel('Memory Used (bytes)')
