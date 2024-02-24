582ea80b86d8f86a1837489643223928bf09ebec
Leshi Chen <leshi@pdx.edu>
2024-02-23 16:58:32 -0800

    added algo comments for tab space optimization

--- final_project/tabulationSpaceOptimization.java
+++ final_project/tabulationSpaceOptimization.java
@@ -1,29 +1,36 @@
 package final_project;
 
 public abstract class tabulationSpaceOptimization {
+    // Method to find the LCS using tabulation with space optimization
     public static String tabulationSpaceOptimizationSolution(String str1, String str2) {
+        // Get the lengths of input strings
         int m = str1.length();
         int n = str2.length();
 
-        // Initializing the dp table
-        String[] prevRow = new String[n + 1];
-        String[] currRow = new String[n + 1];
+        // Initializing the dp table with two rows
+        String[] prevRow = new String[n + 1]; // Previous row
+        String[] currRow = new String[n + 1]; // Current row
+
+        // Initialize both rows with empty strings
         for (int i = 0; i <= n; i++) {
             prevRow[i] = "";
             currRow[i] = "";
         }
 
+        // Fill up the dp table
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++)
-                // If matching
+                // If the characters at current position are equal, include them in the sequence
                 if (str1.charAt(i - 1) == str2.charAt(j - 1))
                     currRow[j] = prevRow[j - 1] + str1.charAt(i - 1);
-                // Not matching
+                // If the characters are not equal, choose the longer common subsequence
                 else
                     currRow[j] = (prevRow[j].length() > currRow[j - 1].length()) ? prevRow[j] : currRow[j - 1];
+            // Update the previous row to be the current row for the next iteration
             prevRow = currRow.clone();
         }
 
+        // Return the LCS of the entire strings
         return currRow[n];
     }
 }
